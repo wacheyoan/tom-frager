64471136fde8306dce3d502c1da69bf2e4840036
Yoan <y.wache@lemon-interactive.fr>
2021-06-16 10:07:02 +0200

    ADD ACF PRO + Logos container

--- wp-content/themes/tomfrager/footer.php
+++ wp-content/themes/tomfrager/footer.php
@@ -24,19 +24,19 @@ echo "<ul class='nav mobile-menu'> <font style='color:red'>Mobile Menu has not b
 }
 ?>
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tomfrager' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tomfrager' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tomfrager' ), 'tomfrager', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+<!--		<div class="site-info">-->
+<!--			<a href="--><?php //echo esc_url( __( 'https://wordpress.org/', 'tomfrager' ) ); ?><!--">-->
+<!--				--><?php
+//				/* translators: %s: CMS name, i.e. WordPress. */
+//				printf( esc_html__( 'Proudly powered by %s', 'tomfrager' ), 'WordPress' );
+//				?>
+<!--			</a>-->
+<!--			<span class="sep"> | </span>-->
+<!--				--><?php
+//				/* translators: 1: Theme name, 2: Theme author. */
+//				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tomfrager' ), 'tomfrager', '<a href="http://underscores.me/">Underscores.me</a>' );
+//				?>
+<!--		</div>-->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ wp-content/themes/tomfrager/front-page.php
@@ -0,0 +1,9 @@
+<?php
+
+get_header();
+
+get_template_part('template-parts/content-logos');
+
+get_sidebar();
+get_footer();
+?>
