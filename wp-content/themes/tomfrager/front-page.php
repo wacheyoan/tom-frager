860caa335d50bc888f5b8c530c9c64c6ebf23a89
wacheyoan <58091401+wacheyoan@users.noreply.github.com>
2021-06-16 11:02:24 +0200

    Develop (#9)
    * Add Jquery + theming
    
    * remove ajax nav
    
    * add woo commerce
    
    * menu en bas pour mobile + gestion des sous-menus
    
    * modif pour que ca fonctionne avec tous les noms de menu
    
    * ADD ACF PRO + Logos container (#7)
    
    Co-authored-by: Yoan <y.wache@lemon-interactive.fr>
    
    * Add Brand container (#8)
    
    Co-authored-by: Yoan <y.wache@lemon-interactive.fr>
    
    Co-authored-by: Yoan <y.wache@lemon-interative.fr>
    Co-authored-by: Yoan <y.wache@lemon-interactive.fr>
    Co-authored-by: gaellebauvin <gaelle_bauvin@ens.univ-artois.fr>

---
+++ wp-content/themes/tomfrager/front-page.php
@@ -0,0 +1,10 @@
+<?php
+
+get_header();
+
+get_template_part('template-parts/content-logos');
+get_template_part('template-parts/content-brand');
+
+get_sidebar();
+get_footer();
+?>
--- wp-content/themes/tomfrager/functions.php
+++ wp-content/themes/tomfrager/functions.php
@@ -143,6 +143,7 @@ function tomfrager_scripts() {
 	wp_enqueue_style( 'tomfrager-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'tomfrager-style', 'rtl', 'replace' );
     wp_enqueue_script('jquery');
+    wp_enqueue_script( 'wdm-mm-toggle', get_stylesheet_directory_uri() . '/js/mobile-menu-toggle.js', array('jquery') );
     wp_enqueue_script('tomfrager-ajax-navigation',get_template_directory_uri().'/js/ajax-navigation.js',array(),_S_VERSION,true);
 	wp_enqueue_script( 'tomfrager-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
@@ -178,3 +179,4 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
