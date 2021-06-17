4c845330aa0f083453619d4f2b08858c6e518a13
wacheyoan <58091401+wacheyoan@users.noreply.github.com>
2021-06-17 15:32:44 +0200

    Add some block especially Instagram (#14)
    Co-authored-by: Yoan <y.wache@lemon-interactive.fr>

--- wp-content/themes/tomfrager/front-page.php
+++ wp-content/themes/tomfrager/front-page.php
@@ -2,9 +2,6 @@
 
 get_header();
 
-get_template_part('template-parts/content-case');
-get_template_part('template-parts/content-logos');
-get_template_part('template-parts/content-brand');
 
 get_sidebar();
 get_footer();
