1d45df5c7f6ad2c411925fe04beaf43f37f2eb3a
gaellebauvin <gaelle_bauvin@ens.univ-artois.fr>
2021-06-17 16:12:19 +0200

    menu no responsive + debut slider

--- wp-content/themes/tomfrager/front-page.php
+++ wp-content/themes/tomfrager/front-page.php
@@ -4,6 +4,7 @@ get_header();
 
 get_template_part('template-parts/content-logos');
 get_template_part('template-parts/content-brand');
+get_template_part('template-parts/content-products');
 
 get_sidebar();
 get_footer();
